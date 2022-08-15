16f76bb39c31f733d27af47e58fa4aeddc5504f3
Emmanuele Bassi <ebassi@gnome.org>
2022-08-15 17:21:50 +0100

    build: Use gnome.post_install()
    Instead of coding the post-installation bits for schemas, desktop/MIME
    entries, and icon cache, we can use the GNOME module inside Meson, which
    will check the binaries we depend on, and will do a better job at
    keeping up to date.

--- build-aux/meson_post_install.py
+++ build-aux/meson_post_install.py
@@ -16,13 +16,3 @@ if not os.path.exists(bindir):
 src = os.path.join(pkgdatadir, application_id)
 dest = os.path.join(bindir, 'gnome-sound-recorder')
 subprocess.call(['ln', '-s', '-f', src, dest])
-
-if not os.environ.get('DESTDIR'):
-    print('Compiling gsettings schemas...')
-    subprocess.call(['glib-compile-schemas', os.path.join(datadir, 'glib-2.0', 'schemas')])
-
-    print('Updating icon cache...')
-    subprocess.call(['gtk-update-icon-cache', '-qtf', os.path.join(datadir, 'icons', 'hicolor')])
-
-    print('Updating desktop database...')
-    subprocess.call(['update-desktop-database', '-q', os.path.join(datadir, 'applications')])
